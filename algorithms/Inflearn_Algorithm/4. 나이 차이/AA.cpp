63c66eef30ea607bb65e5e7f8b4dab25ed2ef3f4
ChanJoon <as06047@yonsei.ac.kr>
2022-08-01 11:44:19 +0900

    Lecuture 04 fixed

--- algorithms/Inflearn_Algorithm/4. 나이 차이/AA.cpp
+++ algorithms/Inflearn_Algorithm/4. 나이 차이/AA.cpp
@@ -3,22 +3,23 @@
 // input.txt 파일을 입력으로 받아 output.txt로 결과 출력
 #include <iostream>
 
+using namespace std;
 int main () {
-	using namespace std;
-	/* freopen("input.txt", "rt", stdin); */
+	
+	/* freopen("input.txt", "rt" , stdin); */ //채점을 위해 주석처리
 	// freopen클래스는 C라이브러리에선 cstdio.h로 사용되었으나 C++에선 iostream으로 대체.
 	// freopen (filename, mode, stream)
 	// "input.txt"파일을 readtxt 모드로 열어 표준입력(standard input)으로 넣는다.
 	
-	int n, a, i, max = -2147000000, min = 2147000000;
+	int n, a, i, max = INT_MIN, min = INT_MAX;
 	
 	cin >> n;
-	for ( i = 1; i <= n; i++ ) {
+	for (i=0; i < n; i++){
 		cin >> a;
 		if ( a > max ) max = a;
 		if ( a < min ) min = a;
 	}
-	cout << max - min;
+	cout << max - min; 
 	
 	return 0;
 }
