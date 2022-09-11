7ebbf7dc12e5568b7ac39147e6bcbcba05718bf0
ChanJoon <as06047@yonsei.ac.kr>
2022-09-12 02:44:11 +0900

    Ch8 ~400pg

--- Ch8.cpp
+++ Ch8.cpp
@@ -128,3 +128,147 @@ void swapv(int a, int b)
 	b = temp;
 }
 #endif
+
+// Reference Properties and Oddities
+#if 0			// Listing 8.5		cubes.cpp
+#include <iostream>
+double cube(double a);
+double refcube(double &ra);
+int main()
+{
+	using namespace std;
+	double x = 3.0;
+	
+	cout << cube(x);
+	cout << " = cube of " << x << endl;
+	cout << refcube(x);
+	cout << " = cube of " << x << endl;
+	return 0;
+}
+
+double cube(double a)
+{
+	a *= a * a;
+	return a;
+}
+
+double refcube(double &ra)
+{
+	ra *= ra * ra;
+	return ra;
+}
+#endif
+
+/* Use const When You Can
+	- Using const protects you against programming erros that inadvertently alter data.
+	- Using const allows a function to process both const and non-const actual arguments, whereas a function that omits const in the prototype only can accept non-const data.
+	- Using a const reference allows the function to generate and use a temporary variable appropriately.
+*/
+
+// Using References with a Structure
+#if 0			// Listing 8.6		strc_ref.cpp
+#include <iostream>
+#include <string>
+struct free_throws
+{
+	std::string name;
+	int made;
+	int attempts;
+	float percent;
+};
+
+void display(const free_throws & ft);
+void set_pc(free_throws & ft);
+free_throws & accumulate(free_throws & target, const free_throws & source);
+
+int main()
+{
+	// partial initializations – remaining members set to 0
+	free_throws one = {"Ifelsa Branch", 13, 14};
+	free_throws two = {"Andor Knott", 10, 16};
+	free_throws three = {"Minnie Max", 7, 9};
+	free_throws four = {"Whily Looper", 5, 9};
+	free_throws five = {"Long Long", 6, 14};
+	free_throws team = {"Throwgoods", 0, 0};
+	
+	// no initialization
+	free_throws dup;
+	
+	set_pc(one);
+	display(one);
+	accumulate(team, one);
+	display(team);
+	
+	// use return value as argument
+	display(accumulate(team, two));
+	accumulate(accumulate(team, three), four);
+	display(team);
+	
+	// use return value in assignment
+	dup = accumulate(team,five);
+	std::cout << "Displaying team:\n";
+	display(team);
+	std::cout << "Displaying dup after assignment:\n";
+	display(dup);
+	set_pc(four);
+	
+	// ill-advised assignment
+	accumulate(dup,five) = four;
+	// accumulate(dup, five);
+	// dup = four;						// overwrite
+	std::cout << "Displaying dup after ill-advised assignment:\n";
+	display(dup);
+	return 0;
+}
+
+void display(const free_throws & ft)
+{
+	using std::cout;
+	cout << "Name: " << ft.name << '\n';
+	cout << " Made: " << ft.made << '\t';
+	cout << "Attempts: " << ft.attempts << '\t';
+	cout << "Percent: " << ft.percent << '\n';
+}
+
+void set_pc(free_throws & ft)
+{
+	if (ft.attempts != 0)
+		ft.percent = 100.0f *float(ft.made)/float(ft.attempts);
+	else
+		ft.percent = 0;
+}
+
+free_throws & accumulate(free_throws & target, const free_throws & source)
+{
+	// return free_throws & => not return a copy but a reference
+	target.attempts += source.attempts;
+	target.made += source.made;
+	set_pc(target);
+	return target;
+}
+#endif
+
+/* Being Careful About What a Return Reference Refers To
+
+const free_throws & clone2(free_throws & ft)
+{
+	free_throws newguy;
+	newguy = ft;
+	return newguy;				// return reference to copy
+}
+SHOULD avoid returning pointers to temporary variables
+THEN return a reference that was passed as an argument to the function.
+
+A second method is to use new to create new storage.
+
+const free_throws & clond(free_throws & ft)
+{
+	free_throws * pt;
+	*pt  = ft;
+	return *pt;
+}
+then use this function
+	free_throws & jolly = clone(three);
+this makes jolly a reference to the new structure. but should use delete to free memory.
+*/
+// 400 pg
