126919a2310323f5d50469f99161dc0e5df63c5a
ChanJoon <as06047@yonsei.ac.kr>
2022-09-15 02:49:45 +0900

    Ch8 ~430pg

--- Ch8.cpp
+++ Ch8.cpp
@@ -421,4 +421,238 @@ A function modifies data in the calling function:
 	- If the data object is a class object
 	=> use a reference	
 */
-// 409 pg
+// 409 pg\
+
+// Default Arguments
+#if 0		// Listing 8.9	left.cpp
+#include <iostream>
+const int ArSize = 80;
+char * left(const char * str, int n = 1);
+int main()
+{
+	using namespace std;
+	char sample[ArSize];
+	cout << "Enter a string:\n";
+	cin.get(sample, ArSize);
+	
+	char * ps = left(sample, 4);
+	cout << ps << endl;
+	delete [] ps;
+	
+	ps = left(sample);
+	cout << ps << endl;
+	delete [] ps;
+	
+	return 0;
+}
+
+char * left(const char * str, int n)
+{
+	if(n < 0)
+		n = 0;
+	char * p = new char[n+1];
+	int i;
+	for (i = 0; i < n && str[i]; i++)		// str[i] reaches the null character, the loop terminates.
+		p[i] = str[i];		// copy characters
+	while (i <= n)
+		p[i++] = '\0';	// set rest of string to '\0'
+	return p;
+}
+#endif
+
+/*	set n to the smaller of the passed value and the string length.
+	int len = strlen(str);
+	n = (n < len) ? n : len;
+	char * p = new char[n+1];
+*/
+
+#if 0			// Listing 8.10	leftover.cpp
+#include <iostream>
+unsigned long left(unsigned long num, unsigned ct);
+char * left(const char * str, int n = 1);
+
+int main()
+{
+	using namespace std;
+	char * trip = "Hawaii!!";
+	unsigned long n = 12345678;
+	int i;
+	char * temp;
+	
+	for (i = 1; i < 10; i++)
+	{
+		cout << left(n, i) << endl;
+		temp = left(trip, i);
+		cout << temp << endl;
+		delete [] temp;		// point to temporary storage
+	}
+	return 0;
+}
+
+// This function returns the first ct digits of the number num
+unsigned long left(unsigned long num, unsigned ct)
+{
+	unsigned digits = 1;
+	unsigned long n = num;
+	
+	if (ct == 0 || num == 0)
+		return 0;
+	while(n /= 10)
+		digits++;
+	if (digits > ct)
+	{
+		ct = digits - ct;
+		while (ct--)
+			num /= 10;
+		return num;
+	}
+	else
+		return num;
+}
+
+// This function return a pointer to a new string
+char * left(const char * str, int n)
+{
+	if(n<0)
+		n = 0;
+	char * p = new char[n+1];
+	int i;
+	for (i = 0; i < n && str[i]; i++)
+		p[i] = str[i];
+	while (i <= n)
+		p[i++] = '\0';
+	return p;
+}
+#endif
+
+// When to Use Function Overloading
+// the same task but with different forms of data.
+
+/* Generic programming; Function Templates
+	template <typename AnyType>
+	void Swap(AnyType &a, AnyType &b)
+	{
+		AnyType temp;
+		temp = a;
+		a = b;
+		b = temp;
+	}
+*/
+// 419 pg
+
+#if 0			// Listing 8.11	funtemp.cpp
+#include <iostream>
+// function template prototype
+template <typename T>	// or class T
+void Swap(T &a, T &b);
+
+int main()
+{
+	using namespace std;
+	int i = 10;
+	int j = 20;
+	
+	cout << "i, j = " << i << ", " << j << ".\n";
+	cout << "Using compiler-generated int swapper: \n";
+	Swap(i, j);
+	cout << "Now i, j = " << i << ", " << j << ".\n";
+	
+	double x = 24.5;
+	double y = 81.7;
+	cout << "x, y = " << x << ", " << y << ".\n";
+	cout << "Using compiler-generated double swapper:\n";
+	Swap(x,y); // generates void Swap(double &, double &)
+	cout << "Now x, y = " << x << ", " << y << ".\n";
+	
+	// cin.get();
+	return 0;
+}
+
+// function template definition
+template <typename T>
+void Swap(T &a, T &b)
+{
+	T temp;		// temp a variable of type T
+	temp = a;
+	a = b;
+	b = temp;
+}
+#endif
+
+#if 0			// Listing 8.13	twoswap.cpp
+#include <iostream>
+template <typename T>
+void Swap(T &a, T &b);
+
+struct job
+{
+	char name[40];
+	double salary;
+	int floor;
+};
+
+// explicit specialization
+template <> void Swap<job> (job &j1, job &j2);
+void Show(job &j);
+
+int main()
+{
+	using namespace std;
+	cout.precision(2);
+	cout.setf(ios::fixed, ios::floatfield);
+	
+	int i = 10, j = 20;
+	cout << "i, j = " << i << ", " << j << ".\n";
+	
+	cout << "Using compiler-generated int swapper: \n";
+	Swap(i, j);		// generates void Swap(int &, int &)
+	cout << "Now i, j = " << i << ", " << j << ".\n";
+	
+	job sue = {"Susan Yaffee", 73000.60, 7};
+	job sidney = {"Sidney Taffee", 78060.72, 9};
+	cout << "Before job swapping:\n";
+	Show(sue);
+	Show(sidney);
+	Swap(sue, sidney);
+	cout << "After job swapping:\n";
+	Show(sue);
+	Show(sidney);
+	// cin.get();
+	return 0;
+}
+
+template <typename T>
+void Swap(T &a, T &b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <> void Swap<job> (job &j1, job &j2)
+{
+	double t1;
+	int t2;
+	t1 = j1.salary;
+	j1.salary = j2.salary;
+	j2.salary = t1;
+	
+	t2 = j1.floor;
+	j1.floor = j2.floor;
+	j2.floor = t2;
+}
+
+void Show(job &j)
+{
+	using namespace std;
+	cout << j.name << ": $" << j.salary << " on floor " << j.floor << endl;
+}
+#endif
+
+/* Instantiations and Specializations
+	When the compiler uses the template to generate a function definition for a particular type, the result is termed an instantiation
+	
+	Implicit instantiations, explicit instantiations, and explicit specializations collectively are termed specializations.
+*/
+// 430pg
