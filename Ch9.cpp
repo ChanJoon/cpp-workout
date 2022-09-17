ffa813c8dbb3a66050d771b144f3452e9e2aef50
ChanJoon <as06047@yonsei.ac.kr>
2022-09-18 01:29:37 +0900

    Ch9 ~459pg

--- Ch9.cpp
+++ Ch9.cpp
@@ -40,3 +40,65 @@ polar rect_to_polar(rect xypos);
 void show_polar(polar dapos);
 
 #endif
+
+// Listing 9.2	file1.cpp
+#include <iostream>
+#include "coordin.h"
+using namespace std;
+int main()
+{
+	rect rplace;
+	polar pplace;
+	
+	cout << "Enter the x and y values: ";
+	while (cin >> rplace.x >> rplace.y)
+	{
+		pplace = rect_to_polar(rplace);
+		show_polar(pplace);
+		cout << "Next two numbers (q to quit): ";
+	}
+	cout << "Bye!\n";
+	return 0;
+}
+
+// Listing 9.3	file2.cpp
+#include <iostream>
+#include <cmath>
+#include "coordin.h"
+
+// conver rectangular to polar coordinates
+polar rect_to_polar(rect xypos)
+{
+	using namespace std;
+	polar answer;
+	
+	answer.distance = sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+	answer.angle  = atan2(xypos.y, xypos.x);
+	return answer;
+}
+
+// show polar coordinates, converting angle to degrees
+void show_polar (polar dapos)
+{
+	using namespace std;
+	const double Rad_to_deg = 57.29577951;
+	
+	cout << "distance = " << dapos.distance;
+	cout << ", angle = " << dapos.angle * Rad_to_deg;
+	cout << " degress\n";
+}
+
+
+/* In Terminal,
+	> g++ file1.cpp file2.cpp -o files
+	> ./files
+	로 여러개의 cpp을 같이 컴파일 하고 link all the resulting "files.exe" files together.
+	
+	> g++ -c files1.cpp
+	> g++ -c files2.cpp
+	> g++ file1.o file2.o
+	> ./.a out
+	로 각각 컴파일하여 object code file을 만들고 실행 파일을 만들 수도 있다.
+*/
+
+// 459pg
