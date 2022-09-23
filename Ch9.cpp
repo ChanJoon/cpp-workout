6b6e5c536257e3b6dbce49c5d6105beac71fcd56
ChanJoon <as06047@yonsei.ac.kr>
2022-09-24 03:42:01 +0900

    Ch9 ~done

--- Ch9.cpp
+++ Ch9.cpp
@@ -318,3 +318,122 @@ int main()
 }
 #endif
 // 481pg
+// Namespace ~491pg
+
+#if 0			// Listing 9.11	namesp.h
+#include <string>
+// create the pers and debts namespaces
+namespace pers
+{
+	struct Person
+	{
+		std::string fname;
+		std::string lname;
+	};
+	void getPerson (Person &);
+	void showPerson (const Person &);
+}
+
+namespace debts
+{
+	using namespace pers;
+	struct Debt
+	{
+		Person name;
+		double amount;
+	};
+	void getDebt(Debt &);
+	void showDebt(const Debt &);
+	double sumDebts(const Debt ar[], int n);
+}
+
+// Listing 9.12	namesp.cpp
+#include <iostream>
+#include "namesp.h"
+
+namespace pers
+{
+	using std::cout;
+	using std::cin;
+	void getPerson(Person & rp)
+	{
+		cout << "Enter first name: ";
+		cin >> rp.fname;
+		cout << "Enter last name: ";
+		cin >> rp.lname;
+	}
+	void showPerson(const Person & rp)
+	{
+		std::cout << rp.lname << ", " << rp.fname;
+	}
+	
+	namespace debts
+	{
+		void getDebt(Debt & rd)
+		{
+			getPerson(rd.name);
+			std::cout << "Enter debt: ";
+			std::cin >> rd.amount;
+		}
+		void showDebt(const Debt & rd)
+		{
+			showPerson(rd.name);
+			std::cout << ": $" << rd.amount << std::endl;
+		}
+		double sumDebts(const Debt ar[], int n)
+		{
+			double total = 0;
+			for (int i = 0; i < n; i++)
+				total += ar[i].amount;
+			return total;
+		}
+	}
+}
+
+// Listing 9.13 usenmsp.cpp
+// usenmsp.cpp
+#include <iostream>
+#include "namesp.h"
+
+void other(void);
+void another(void);
+int main(void)
+{
+	using debts::Debt;
+	
+	using debts::showDebt;
+	Debt golf = { {"Benny", "Goatsniff"}, 120.0};
+	showDebt(golf);
+	other();
+	another();
+	
+	return 0;
+}
+
+void other(void)
+{
+	using std::cout;
+	using std::endl;
+	using namespace debts;
+	Person dg = {"Doodles", "Glister"};
+	showPerson(dg);
+	
+	cout << endl;
+	Debt zippy[3];
+	 int i;
+	 for (i = 0; i < 3; i++)
+		 getDebt(zippy[i]);
+	 for (i = 0; i < 3; i++)
+		 showDebt(zippy[i]);
+	 cout << "Total debt: $" << sumDebts(zippy, 3) << endl;
+	 return;
+}
+
+void another(void)
+{
+	using pers::Person;
+	Person collector = {"Milo", "Rightshift"};
+	pers::showPerson(collector);
+	std::cout << std::endl;
+}
+#endif
