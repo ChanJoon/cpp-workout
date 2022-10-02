3993e7f0084389c1b005eb4b985de32335c7e8f9
ChanJoon <as06047@yonsei.ac.kr>
2022-10-03 05:41:42 +0900

    Ch10 ~Done

--- Ch10.cpp
+++ Ch10.cpp
@@ -279,3 +279,229 @@ int main()
 	return 0;
 }
 #endif
+
+// Listing10.7		stock20.h
+#ifndef STOCK20_H_
+#define STOCK20_H_
+#include <string>
+
+class Stock
+{
+	private:
+		std::string company;
+		int shares;
+		double share_val;
+		double total_val;
+		void set_tot() { total_val = shares* share_val; }
+	public:
+		Stock();
+		Stock(const std::string & co, long n = 0, double pr = 0.0);
+		~Stock();
+		void buy(long num, double price);
+		void sell(long num, double price);
+		void update(double price);
+		void show()const;
+		const Stock & topval(const Stock & s) const;
+};
+
+#endif
+
+#if 0		//Listing10.8		stock20.cpp
+#include <iostream>
+#include "stock20.h"
+
+Stock::Stock()
+{
+	company = "no name";
+	shares = 0;
+	share_val = 0.0;
+	total_val = 0.0;
+}
+
+Stock::Stock(const std::string & co, long n, double pr)
+{
+	company = co;
+	if (n < 0)
+	{
+		std::cout << "Number of shares can’t be negative; "
+		<< company << " shares set to 0.\n";
+		shares = 0;
+	}
+	else
+		shares = n;
+	share_val = pr;
+	set_tot();
+}
+
+// class destructor
+Stock::~Stock() // quiet class destructor
+{
+}
+// other methods
+void Stock::buy(long num, double price)
+{
+	if (num < 0)
+	{
+		std::cout << "Number of shares purchased can’t be negative. "
+		<< "Transaction is aborted.\n";
+	}
+	else
+	{
+		shares += num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::sell(long num, double price)
+{
+	using std::cout;
+	if (num < 0)
+	{
+		cout << "Number of shares sold can’t be negative. "
+		<< "Transaction is aborted.\n";
+	}
+	else if (num > shares)
+	{
+		cout << "You can’t sell more than you have! "
+		<< "Transaction is aborted.\n";
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+}
+
+void Stock::show() const
+{
+	using std::cout;
+	using std::ios_base;
+
+	// set format to #.###
+	ios_base::fmtflags orig =
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	std::streamsize prec = cout.precision(3);
+	cout << "Company: " << company << " Shares: " << shares << '\n';
+	cout << " Share Price: $" << share_val;
+
+	// set format to #.##
+	cout.precision(2);
+	cout << " Total Worth: $" << total_val << '\n';
+
+	// restore original format
+	cout.setf(orig, ios_base::floatfield);
+	cout.precision(prec);
+}
+
+
+// New function Stock::topval
+const Stock & Stock::topval(const Stock & s) const
+{
+	if (s.total_val > total_val)
+		return s;
+	else
+		return *this;
+}
+#endif
+
+#if 0		// Listing10.9	usestock2.cpp
+// compile with stock20.cpp
+#include <iostream>
+#include "stock20.h"
+
+const int STKS = 4;
+int main()
+{
+	Stock stocks[STKS] = {
+		Stock("NanoSmart", 12, 20.0),
+		Stock("Boffo Objects", 200, 2.0),
+		Stock("Monolithic Obelisks", 130, 3.25),
+		Stock("Fleep Enterprises", 60, 6.5)
+	};
+
+	std::cout << "Stock holdings:\n";
+
+	int st;
+	for (st = 0; st < STKS; st++)
+		stocks[st].show();
+
+	// set pointer to first element
+	const Stock * top = &stocks[0];
+
+	for (st = 1; st < STKS; st++)
+		top = &top -> topval(stocks[st]);
+	std::cout << "\nMost valuable holding:\n";
+	top -> show();
+	return 0;
+}
+
+#endif
+
+// Listing10.10	stack.h
+#ifndef STACK_H_
+#define STACK_H_
+
+typedef unsigned long Item;
+
+class Stack
+{
+	private:
+		enum {MAX = 10};
+		Item items[MAX];
+		int top;
+	public:
+		Stack();
+		bool isempty() const;
+		bool isfull() const;
+		bool push(const Item & item);
+		bool pop(Item & item);
+};
+#endif
+
+#if 0 // Listing 10.11	stack.cpp
+#include "stack.h"
+Stack::Stack()
+{
+	top = 0;
+}
+
+bool Stack::isempty() const
+{
+	return top == 0;
+}
+
+bool Stack::isfull() const
+{
+	return top == MAX;
+}
+
+bool Stack::push(const Item & item)
+{
+	if (top < MAX)
+	{
+		items[top++] = item;
+		return true
+	}
+	else
+		return false;
+}
+
+bool Stack::pop(Item & item)
+{
+	if (top > 0)
+	{
+		item = items[--top];
+		return true;
+	}
+	else
+		return false;
+}
+#endif
