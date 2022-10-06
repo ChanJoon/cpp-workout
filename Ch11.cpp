64d033221fa568123bee0bb5830a0f19da18e8ab
ChanJoon <as06047@yonsei.ac.kr>
2022-10-07 02:36:54 +0900

    Ch11 ~577pg

---
+++ Ch11.cpp
@@ -0,0 +1,10 @@
+/* Ch11 Working with Classes
+- Operator overloading
+- Friend functions
+- Overloading the << operator for output
+- State members
+- Using rand() to generate random values
+- Automatic conversions and type casts for classes
+- Class conversion functions
+*/
+
